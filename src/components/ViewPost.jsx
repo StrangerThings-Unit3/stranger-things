5f18ce8dedfe1d1bc11bdd3faa736fc47cfb110c
Yasaman Esmaeili <yasamanesmaeili@Yasamans-MacBook-Air.local>
2023-08-16 23:18:52 -0400

    ViewPost has been created

--- src/components/ViewPost.jsx
+++ src/components/ViewPost.jsx
@@ -1,1 +1,28 @@
-// Create
+import { useEffect, useState } from "react";
+
+// Function to hold the post data
+function ViewPost (postId) {
+    const [posts, setPosts] = useState([]);
+    useEffect(() =>{
+        const fetchPosts = async () => {
+            try{
+                const response = await ViewPost(postId);
+                setPosts(response);
+            } catch (error) {
+                console.log('Error fetching post:', error);
+            }
+        };
+fetchPosts();
+}, []);
+return (
+  <div className='container'>
+    <div className='View Post'>
+    {posts.map((post) => {
+          return <PostItem key={post._id} post={post} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ViewPost;
